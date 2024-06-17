19af057ea96da18c36d5a88ef3683ee17d6f259d
B VENUGOPAL <133234255+BisaVenugopal@users.noreply.github.com>
2024-06-17 21:41:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,9 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api",router)
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
 
 const PORT = 4000 || process.env.PORT
 connectDB().then(() => {
